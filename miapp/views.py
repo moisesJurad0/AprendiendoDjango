e731cf363c6113b7853801ea33b2bcfded329c02
Moisés Jurado <moises003@gmail.com>
2023-02-03 18:38:20 -0500

    hello world

--- miapp/views.py
+++ miapp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+# MVC = Modelo Vista Controlador
+# MVT = Modelo Vista Template
+
+
+def hola_mundo(request):
+    return HttpResponse("ESA GENTE")
